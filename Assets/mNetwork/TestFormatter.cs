ad7d6a4a0159149a1521ee1a420110e47e7e8062
Brandon Edwards <brandon.edwards1@live.co.uk>
2016-06-24 16:05:36 +0100

    Netcode Optimisation part 1
    Just optimised the netcode... no longer does it send super huge 1kb
    messages. they are now only 500b in size
    
    I FIXED IT YYYYYYYYYEEEEEEEEEEEAAAAAAAAAAHHHHHHH!

---
+++ Assets/mNetwork/TestFormatter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class TestFormatter : MonoBehaviour {
+
+	byte[] data;
+	public string receivedData;
+	// Use this for initialization
+	void Start () {
+		SerialiseData ();
+		DeserialiseData ();
+	}
+
+
+	void SerialiseData () {
+		using (Stream stream = new MemoryStream()) {
+			BinaryFormatter formatter = new BinaryFormatter();
+			formatter.Serialize(stream,"MEGA TEST STRING RAAAAWRRRR");
+			data = new byte[stream.Length];
+			stream.Seek(0,SeekOrigin.Begin);
+			stream.Read(data,0,(int)stream.Length);
+		}
+	}
+
+	void DeserialiseData () {
+		using (Stream stream = new MemoryStream(data,0,data.Length)) {
+			BinaryFormatter formatter = new BinaryFormatter();
+			//stream.Seek(0,SeekOrigin.Begin);
+			receivedData = (string)formatter.Deserialize(stream);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
